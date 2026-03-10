b452d68fdad38178320063755648494bac55ebdb
Gar/ <manjimichaelgar@gmail.com>
2026-03-10 22:08:35 +0100

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
-<<<<<<< HEAD
-=======
   serverExternalPackages: ['pg', 'typeorm', 'reflect-metadata'],
->>>>>>> feat/custom-integration
 
   allowedDevOrigins: ['localhost', '127.0.0.1', '::1'],
   /* config options here */
